88e55a8255541107339fbe6fba658f02970a171a
UltraHispeed-Elite <62296749+UltraHispeed-Elite@users.noreply.github.com>
2023-11-13 08:02:59 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,58 +1,80 @@
- var player;
- var p_flicker = false;
- var flickerCount = 0;
+var player;
+var playerX, playerY;
+var pX, pY;
 
-function setup() {
-  createCanvas(400,400);
-  
-  player = createSprite(200, 200, 50, 50);
-}
+var bg;
 
-function draw() {
-  background("black");
-  
-  if(keyDown("6")) {
-    p_flicker = true;
+var screen_1 = function(sketch) {
+  sketch.setup = function() {
+    let canvas1 = sketch.createCanvas(400, 400);
+    canvas1.position(0,0);
+    
+    player = sketch.createSprite(200,110, 50, 50);
   }
   
-  playerMovement();
-  playerFlicker();
-  
-  drawSprites();
-}
-
-function playerMovement() {
-  if(keyDown("w")){
-    player.y -= 5;
+  sketch.draw = function() {
+    sketch.background("black");
+    
+    sketch.camera.x = player.x;
+    sketch.camera.y = player.y;
+    
+    sketch.playerMovement();
+    
+    sketch.drawSprites();
   }
   
-  if(keyDown("a")){
-    player.x -= 5;
-  }
-  
-  if(keyDown("s")){
-    player.y += 5;
-  }
-  
-  if(keyDown("d")){
-    player.x += 5;
-  }
-}
-
-function playerFlicker(){
-  if(p_flicker === true) {
-    if(flickerCount < 5) {
-      if(frameCount % 4 === 0) {
-        player.visible = false;
-        flickerCount += 0.5;
-      }else {
-        player.visible = true;
-      }
-    }else {
-      p_flicker = false;
+  sketch.playerMovement = function(){
+    if(sketch.keyDown("w")) {
+      player.y -= 5;
     }
-  }else {
-    flickerCount = 0;
-    player.visible = true;
+  
+    if(sketch.keyDown("a")) {
+      player.x -= 5;
+    }
+  
+    if(sketch.keyDown("s")) {
+      player.y += 5;
+    }
+  
+    if(sketch.keyDown("d")) {
+      player.x += 5;
+    }
+    
+    playerX = player.x;
+    playerY = player.y;
+    
+    console.log(playerX, playerY);
   }
 }
+
+new p5(screen_1);
+
+var screen_2 = function(sketch) {
+  sketch.preload = function() {
+    bg = sketch.loadImage("outline_background.png");
+  }
+  
+  sketch.setup = function() {
+    let canvas1 = sketch.createCanvas(400, 400);
+    canvas1.position(0,0);
+  }
+  
+  sketch.draw = function() {
+    sketch.background(bg);
+    
+    if(sketch.frameCount % 1 === 0) {
+      sketch.clear();
+    }
+    
+    pX = playerX;
+    pY = playerY;
+    
+    sketch.text(pX, 50, 50);
+    sketch.text(pY, 75, 50);
+    
+    
+    sketch.drawSprites();
+  }
+}
+
+new p5(screen_2);
